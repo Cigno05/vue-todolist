de55a1c8133c345f024fd67123315fb96c574635
Cigno05 <paolocalcagni.82@gmail.com>
2024-03-27 17:06:22 +0100

    list print in HTML, text, line-through

--- js/app.js
+++ js/app.js
@@ -5,11 +5,44 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        toDoArray: [
+          {
+            text: 'Fare colazione',
+            done: false
+          },
+          {
+            text: 'Vestirsi',
+            done: false
+          },
+          {
+            text: 'Uscire di casa',
+            done: false
+          }
+        ],
+        inputText:'',
+        listDone: false
       }
     },
     methods: {
 
+      addToDo() {
+        if (this.inputValue !== '') {
+          this.toDoArray.push(
+            {
+              text: this.inputText,
+              done: false
+            }
+          )
+          this.inputText = ''
+        }
+        
+
+      },
+      toDoDone(i) {
+        this.toDoArray[i].done = true
+        console.log(this.toDoArray[i].done)
+        
+      },
     },
     
   }).mount('#app')
